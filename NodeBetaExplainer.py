dccc1ca081a541b2fcebe42722c03d808a61aad1
wsloneker <63076443+wsloneker@users.noreply.github.com>
2025-06-05 11:25:49 -0400

    Update NodeBetaExplainer.py

--- NodeBetaExplainer.py
+++ NodeBetaExplainer.py
@@ -10,11 +10,15 @@ from pyro.infer import SVI, Trace_ELBO
 class BetaExplainer:
     '''This is a post-hoc explainer based on the Beta Distribution.'''
     def __init__(self, model, X, G, device, a, b):
-        '''Initialization of the model.'''
+        '''
+            Initialization of the model. We take the model (model), original features (X), original edge index (G), device to save to (device),
+            alpha parameter for the Beta distribution (a), and beta parameter for the Beta distribution (b).
+        '''
         self.model = model
         self.X = X
         self.G = G
         self.device = device
+        # get model output on dataset
         with torch.no_grad():
             self.target = self.model(self.X, self.G)
             self.target = self.target.flatten()
@@ -27,6 +31,7 @@ class BetaExplainer:
         self.b = b
 
     def model_p(self, ys):
+        ''' 'True' Distribution to be approximated '''
         alpha = self.a * torch.ones(self.N).to(self.device)
         beta = self.b * torch.ones(self.N).to(self.device)
         alpha_edges = alpha[self.G[0, :]]
@@ -39,6 +44,7 @@ class BetaExplainer:
             pyro.sample("obs", dist.Categorical(preds), obs=ys)
 
     def guide(self, ys):
+        ''' Approximation of the model (IE model_p) for tractability '''
         alpha = pyro.param("alpha_q", self.a * torch.ones(self.N).to(self.device), constraint=constraints.positive)
         beta = pyro.param("beta_q", self.b * torch.ones(self.N).to(self.device), constraint=constraints.positive)
         alpha_edges = alpha[self.G[0, :]]
@@ -49,14 +55,16 @@ class BetaExplainer:
         init.exp().flatten()
 
     def train(self, epochs: int, lr: float = 0.0005):
+        ''' Model training function '''
         adam_params = {"lr": lr, "betas": (0.90, 0.999)}
         optimizer = Adam(adam_params)
         svi = SVI(self.model_p, self.guide, optimizer, loss=Trace_ELBO())
 
         elbos = []
         for epoch in range(epochs):
-            ys = torch.distributions.categorical.Categorical(self.target.exp()).sample(torch.Size([self.obs]))
-            elbo = svi.step(ys)
+            ys = torch.distributions.categorical.Categorical(self.target.exp()).sample(torch.Size([self.obs])) # get original vs predicted model output on the masked edge index
+            elbo = svi.step(ys) # elbo = loss --> maximized to indirectly decrase KL divergence between original model output and model output on masked graph
+            # we want our masked edge index output to look similar to original model output
             elbos.append(elbo)
             self.loss.append([elbo, epoch, epochs])
             if epoch > 249:
@@ -65,6 +73,7 @@ class BetaExplainer:
         clear_masks(self.model)
 
     def edge_mask(self):
+        # Get the edge mask!
         m = torch.distributions.beta.Beta(pyro.param("alpha_q").detach()[self.G[0, :]], pyro.param("beta_q").detach()[self.G[1, :]]).sample(torch.Size([10000]))
         return m.mean(dim=0)
 
